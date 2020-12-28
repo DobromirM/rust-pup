55a3e3984d50a01e01a0e7e2c30906fd3c5e9491
DobromirM <mr.d.marinov@gmail.com>
2020-12-28 15:49:22 +0000

    * Removed awkward resizing.

--- src/visualise.rs
+++ src/visualise.rs
@@ -1,5 +1,5 @@
 use std::{error, fmt};
-use viuer::{print_from_file, Config};
+use viuer::print_from_file;
 
 #[derive(Debug, Clone)]
 pub(crate) struct VisualisationError;
@@ -13,22 +13,6 @@ impl fmt::Display for VisualisationError {
 }
 
 pub(crate) fn draw_image(file_path: &str) -> Result<(), VisualisationError> {
-    let terminal_size = get_terminal_size();
-
-    let conf = Config {
-        width: Some(terminal_size.0),
-        height: Some(terminal_size.1),
-        ..Default::default()
-    };
-
-    print_from_file(file_path, &conf).map_err(|_| VisualisationError)?;
+    print_from_file(file_path, &Default::default()).map_err(|_| VisualisationError)?;
     Ok(())
-}
-
-fn get_terminal_size() -> (u32, u32) {
-    if let Some((w, h)) = term_size::dimensions() {
-        (w as u32, h as u32)
-    } else {
-        (80, 45)
-    }
 }
